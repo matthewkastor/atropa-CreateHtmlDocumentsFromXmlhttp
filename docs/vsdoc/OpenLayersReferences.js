94388334d7ba6d228ed575dc6e9fc5f9c0615272
Matthew Kastor <matthewkastor@gmail.com>
2014-01-01 13:29:21 -0500

    updated docs

--- docs/vsdoc/OpenLayersReferences.js
+++ docs/vsdoc/OpenLayersReferences.js
@@ -1,4 +1,7 @@
 
 /// <reference path="atropa-vsdoc.js" />
 /// <reference path="atropa.CreateHtmlDocumentsFromXmlhttp-vsdoc.js" />
+/// <reference path="atropa.data-vsdoc.js" />
+/// <reference path="atropa.HTMLParser-vsdoc.js" />
+/// <reference path="atropa.Requester-vsdoc.js" />
 
--- docs/vsdoc/symbols/atropa-vsdoc.js
+++ docs/vsdoc/symbols/atropa-vsdoc.js
@@ -6,14 +6,35 @@
 
     window.atropa = {
         /// <summary></summary>
-        /// <field name="Requester" type="">Required module, the docs for it are in the &lt;code&gt;
-        ///  atropa-Requester/docs&lt;/code&gt; directory where this module 
-        ///  is located.</field>
-        /// <field name="HTMLParser" type="">Required module, the docs for it are in the &lt;code&gt;
-        ///  atropa-HTMLParser/docs&lt;/code&gt; directory where this module 
+        /// <field name="data" type="">Container for gobal data related to the classes and functions.</field>
+        /// <field name="ArgsInfo" type="">Required module, the docs for it are in the &lt;code&gt;
+        ///  atropa-ArgsInfo/docs&lt;/code&gt; directory where this module 
         ///  is located.</field>
         /// <returns type="atropa"/>
                 
+        supportCheck: function(className, errorMessage) {
+            /// <summary>Checks whether this class has been marked as unsupported and throws an 
+            ///  error if it has.</summary>
+            /// <param name="className" type="String">The name of the class.</param>
+            /// <param name="errorMessage" type="String">Optional. A custom error message. Defaults to
+            ///  atropa.data[className].error</param>
+        }, 
+        
+        requires: function(className, requirementFn, errorMessage) {
+            /// <summary>Pushes a requirement check into atropa.data.requirements. The test
+            ///  tests whether the class is supported in this environment. Sets
+            ///  atropa.data[className]&apos;s support to unsupported and error to errorMessage
+            ///  if the requirementFn returns false. The requirement checks will all be run
+            ///  after the library has loaded.</summary>
+            /// <param name="className" type="String">The name of the class.</param>
+            /// <param name="requirementFn" type="Function">A function to test whether or not the class
+            ///  is supported in this environment. If supported, returns true otherwise
+            ///  return false.</param>
+            /// <param name="errorMessage" type="String">The error message to use when this class or its
+            ///  methods are called in unsupported environments. Defaults to:
+            ///  &apos;The atropa.&apos; + className + &apos; class is unsupported in this environment.&apos;;</param>
+        }
+        
     };
 
     var $x = window.atropa;
